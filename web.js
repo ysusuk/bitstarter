aeaee682ba6cc3231796bb3fe8cbf484e97f4511
Iurii Susuk <yurasusuk@gmail.com>
2013-07-25 08:58:07 +0000

    web.js read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
